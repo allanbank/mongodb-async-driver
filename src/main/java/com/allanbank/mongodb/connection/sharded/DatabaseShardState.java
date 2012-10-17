a6ae4d5bb82e6fcc255f34528fed3ad7e0344e06
Robert J. Moore <Robert.J.Moore@allanbank.com>
2012-10-17 16:30:51 -0400

    Initial state tracking for what data exists on what shard.

---
+++ src/main/java/com/allanbank/mongodb/connection/sharded/DatabaseShardState.java
@@ -0,0 +1,167 @@
+/*
+ * Copyright 2012, Allanbank Consulting, Inc. 
+ *           All Rights Reserved
+ */
+
+package com.allanbank.mongodb.connection.sharded;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import com.allanbank.mongodb.bson.Document;
+
+/**
+ * DatabaseShardState provides the state of the database with respect to
+ * sharding.
+ * 
+ * @api.no This class is <b>NOT</b> part of the drivers API. This class may be
+ *         mutated in incompatible ways between any two releases of the driver.
+ * @copyright 2012, Allanbank Consulting, Inc., All Rights Reserved
+ */
+public class DatabaseShardState {
+
+    /** The mapping of collection names to their states. */
+    private final ConcurrentMap<String, CollectionShardState> myCollections;
+
+    /**
+     * The primary shard for the database. This is where we can find non-sharded
+     * collections.
+     */
+    private volatile Shard myPrimaryShard;
+
+    /** If the system thinks that the database supports sharding. */
+    private volatile boolean mySharded;
+
+    /**
+     * Creates a new DatabaseShardState.
+     * 
+     * @param sharded
+     *            If true the database supports sharding.
+     * @param primaryShard
+     *            The primary shard for the database. This is where we can find
+     *            non-sharded collections.
+     */
+    public DatabaseShardState(final boolean sharded, final Shard primaryShard) {
+        mySharded = sharded;
+        myPrimaryShard = primaryShard;
+        myCollections = new ConcurrentHashMap<String, CollectionShardState>();
+    }
+
+    /**
+     * Tries to determine the correct shard to receive the query/inserted
+     * document.
+     * 
+     * @param collection
+     *            The collection that the query or document is going to be used
+     *            with.
+     * @param queryOrDocument
+     *            The query that will be used or the document that
+     * @return The shard to use with the query/insert or <code>null</code> if a
+     *         specific shard could not be determined.
+     */
+    public Shard findShardFor(final String collection,
+            final Document queryOrDocument) {
+
+        Shard result = myPrimaryShard;
+
+        // If sharded/partitioned ...
+        if (mySharded) {
+            // ... use the CollectionShardState for the collection...
+            final CollectionShardState collectionState = myCollections
+                    .get(collection);
+            if (collectionState != null) {
+                result = collectionState.findShard(queryOrDocument);
+            }
+            else {
+                // Return null since we don't know about the collection.
+                result = null;
+
+                // TODO: Request the state for the collection.
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Deletes the state of for the specified database/collection. This should
+     * be called when a
+     * {@link com.allanbank.mongodb.error.ShardConfigStaleException} is
+     * encountered.
+     * 
+     * @param collection
+     *            The collection that the query or document is going to be used
+     *            with.
+     */
+    public void deleteState(final String collection) {
+        myCollections.remove(collection);
+    }
+
+    /**
+     * Returns the state of the collections within the database.
+     * 
+     * @return The state of the collections within the database.
+     */
+    public Map<String, CollectionShardState> getCollections() {
+        return Collections.unmodifiableMap(myCollections);
+    }
+
+    /**
+     * Returns the primary shard for the database. This is where we can find
+     * non-sharded collections.
+     * 
+     * @return The primary shard for the database. This is where we can find
+     *         non-sharded collections.
+     */
+    public Shard getPrimaryShard() {
+        return myPrimaryShard;
+    }
+
+    /**
+     * Returns true the system thinks that the database supports sharding.
+     * 
+     * @return True the system thinks that the database supports sharding.
+     */
+    public boolean isSharded() {
+        return mySharded;
+    }
+
+    /**
+     * Sets the state of the collections within the database.
+     * 
+     * @param collections
+     *            The state of the collections.
+     */
+    public void setCollections(
+            final Map<String, CollectionShardState> collections) {
+        myCollections.clear();
+        if (collections != null) {
+            myCollections.putAll(collections);
+        }
+    }
+
+    /**
+     * Sets the value of primary shard to the new value. This is where we can
+     * find non-sharded collections.
+     * 
+     * @param primaryShard
+     *            The new value for the primary shard.
+     */
+    public void setPrimaryShard(final Shard primaryShard) {
+        myPrimaryShard = primaryShard;
+    }
+
+    /**
+     * Sets to true if true the system thinks that the database supports
+     * sharding.
+     * 
+     * @param sharded
+     *            The new value for if the system thinks that the database
+     *            supports sharding.
+     */
+    public void setSharded(final boolean sharded) {
+        mySharded = sharded;
+    }
+}
---
+++ src/main/java/com/allanbank/mongodb/connection/sharded/ShardedClusterState.java
@@ -0,0 +1,141 @@
+/*
+ * Copyright 2012, Allanbank Consulting, Inc. 
+ *           All Rights Reserved
+ */
+
+package com.allanbank.mongodb.connection.sharded;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import com.allanbank.mongodb.MongoDbConfiguration;
+import com.allanbank.mongodb.bson.Document;
+import com.allanbank.mongodb.connection.state.ClusterState;
+
+/**
+ * ShardedClusterState provides an extension to the cluster state that
+ * understands how to route requests to shards based on the query/document of
+ * interest.
+ * 
+ * @copyright 2012, Allanbank Consulting, Inc., All Rights Reserved
+ */
+public class ShardedClusterState extends ClusterState {
+
+    /**
+     * The URIs to pass to the shard servers as the configuration mongod
+     * locations.
+     */
+    private final String myConfigDatabases;
+
+    /** The state of the databases in the cluster. */
+    private final ConcurrentMap<String, DatabaseShardState> myDatabases;
+
+    /**
+     * Creates a new ShardedClusterState.
+     * 
+     * @param configDatabases
+     *            The string to pass to the shard servers as the configuration
+     *            mongod locations.
+     * @param config
+     *            The configuration of this MongoDB client.
+     */
+    public ShardedClusterState(final String configDatabases,
+            final MongoDbConfiguration config) {
+        super(config);
+
+        myConfigDatabases = configDatabases;
+        myDatabases = new ConcurrentHashMap<String, DatabaseShardState>(
+                config.getMaxConnectionCount() * 2);
+    }
+
+    /**
+     * Tries to determine the correct shard to receive the query/inserted
+     * document.
+     * 
+     * @param database
+     *            The database that the query or document is going to be used
+     *            with.
+     * @param collection
+     *            The collection that the query or document is going to be used
+     *            with.
+     * @param queryOrDocument
+     *            The query that will be used or the document that
+     * @return The shard to use with the query/insert or <code>null</code> if a
+     *         specific shard could not be determined.
+     */
+    public Shard findShardFor(final String database, final String collection,
+            final Document queryOrDocument) {
+
+        Shard result = null;
+
+        // If we know about the database, then delegate to the
+        // DatabaseShardState
+        final DatabaseShardState dbState = myDatabases.get(database);
+        if (dbState != null) {
+            result = dbState.findShardFor(collection, queryOrDocument);
+        }
+
+        // ... otherwise TODO request the database information.
+
+        // Use the mongos for now.
+        return result;
+    }
+
+    /**
+     * Deletes the state of for the specified database/collection. This should
+     * be called when a
+     * {@link com.allanbank.mongodb.error.ShardConfigStaleException} is
+     * encountered.
+     * 
+     * @param database
+     *            The database that the query or document is going to be used
+     *            with.
+     * @param collection
+     *            The collection that the query or document is going to be used
+     *            with.
+     */
+    public void deleteState(final String database, final String collection) {
+
+        // If we know about the database, then delegate to the
+        // DatabaseShardState
+        final DatabaseShardState dbState = myDatabases.get(database);
+        if (dbState != null) {
+            dbState.deleteState(collection);
+        }
+    }
+
+    /**
+     * Returns the state of the databases within the cluster.
+     * 
+     * @return The state of the databases within the cluster.
+     */
+    public Map<String, DatabaseShardState> getCollections() {
+        return Collections.unmodifiableMap(myDatabases);
+    }
+
+    /**
+     * Returns the URIs to pass to the shard servers as the configuration mongod
+     * locations.
+     * 
+     * @return The URIs to pass to the shard servers as the configuration mongod
+     *         locations.
+     */
+    public String getConfigDatabases() {
+        return myConfigDatabases;
+    }
+
+    /**
+     * Sets the state of the databases within the cluster.
+     * 
+     * @param databases
+     *            The state of the databases.
+     */
+    public void setDataabses(final Map<String, DatabaseShardState> databases) {
+        myDatabases.clear();
+        if (databases != null) {
+            myDatabases.putAll(databases);
+        }
+    }
+}
