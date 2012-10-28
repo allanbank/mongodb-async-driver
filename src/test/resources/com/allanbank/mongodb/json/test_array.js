e629bb60bf0f85f7a5bcc6c373e0a274e0934ac5
Robert J. Moore <Robert.J.Moore@allanbank.com>
2012-10-28 17:58:44 -0400

    Simple parser for JSON documents into Documents using JavaCC.

---
+++ src/test/resources/com/allanbank/mongodb/json/test_array.js
@@ -0,0 +1,28 @@
+[
+    false, true, null, 1, 1.0, 1.0e12, 1e-1, 'abc', "def", ghi, 
+    { 
+        int : 1,
+        double : 1.0,
+        double_1 : 1.0e12,
+        double_2 : 1e-1,
+        string : 'abc',
+        string2 : "def",
+        symbol : ghi, 
+        array : [
+            1, 1.0, 1.0e12, 1e-1, 'abc', "def", ghi
+        ],
+        doc : {
+            int : 1,
+            double : 1.0,
+            double_1 : 1.0e12,
+            double_2 : 1e-1,
+            string : 'abc',
+            string2 : "def",
+            symbol : ghi,
+            array : [
+                1, 1.0, 1.0e12, 1e-1, 'abc', "def", ghi
+            ]
+        }
+    },
+    [  1, 1.0, 1.0e12, 1e-1, 'abc', "def", ghi ]
+]
