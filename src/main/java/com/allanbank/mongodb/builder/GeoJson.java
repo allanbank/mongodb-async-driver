d8ea8a46564a58e40a7f3ccf9a322a76a90c2607
Robert J. Moore <Robert.J.Moore@allanbank.com>
2013-03-06 23:04:58 -0500

    GeoJSON document construction support.

---
+++ src/main/java/com/allanbank/mongodb/builder/GeoJson.java
@@ -0,0 +1,395 @@
+/*
+ * Copyright 2013, Allanbank Consulting, Inc. 
+ *           All Rights Reserved
+ */
+
+package com.allanbank.mongodb.builder;
+
+import java.awt.Point;
+import java.awt.geom.Point2D;
+import java.util.List;
+
+import com.allanbank.mongodb.bson.Document;
+import com.allanbank.mongodb.bson.builder.ArrayBuilder;
+import com.allanbank.mongodb.bson.builder.BuilderFactory;
+import com.allanbank.mongodb.bson.builder.DocumentBuilder;
+
+/**
+ * GeoJson provides static methods to help create <a
+ * href="http://www.geojson.org/geojson-spec.html">GeoJSON</a> BSON Documents.
+ * <p>
+ * This class uses the {@link Point} and {@link Point2D} classes to represent
+ * GeoJSON positions. To assist in converting raw (x, y) coordinates the
+ * {@link #p(double, double)} and {@link #p(int, int)} methods are provides to
+ * quickly construct the Point instances.
+ * </p>
+ * <p>
+ * As an example of using this class consider the following Polygon with a hole
+ * from the GeoJSON specification: <blockquote>
+ * 
+ * <pre>
+ * <code>
+ * { "type": "Polygon",
+ *   "coordinates": [
+ *     [ [100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0] ],
+ *     [ [100.2, 0.2], [100.8, 0.2], [100.8, 0.8], [100.2, 0.8], [100.2, 0.2] ]
+ *     ]
+ *  }
+ * </code>
+ * </pre>
+ * 
+ * </blockquote>
+ * 
+ * The equivalent BSON document can be constructed via:<blockquote>
+ * 
+ * <pre>
+ * <code>
+ * import static com.allanbank.mongodb.builder.GeoJson.polygon;
+ * import static com.allanbank.mongodb.builder.GeoJson.p;
+ * 
+ * Document geoJsonPolygon = polygon(
+ *      Arrays.asList( p(100.0, 0.0), p(101.0, 0.0), p(101.0, 1.0), p(100.0, 1.0), p(100.0, 0.0) ),
+ *      Arrays.asList( p(100.2, 0.2), p(100.8, 0.2), p(100.8, 0.8), p(100.2, 0.8), p(100.2, 0.2) ) );
+ * </code>
+ * </pre>
+ * 
+ * </blockquote>
+ * 
+ * @see <a href="http://www.geojson.org/geojson-spec.html">The GeoJSON Format
+ *      Specification</a>
+ * @copyright 2013, Allanbank Consulting, Inc., All Rights Reserved
+ */
+public final class GeoJson {
+    /** The class for an integer {@link Point} */
+    protected static final Class<Point> POINT_CLASS = Point.class;
+
+    /**
+     * Constructs a GeoJSON 'LineString' document from the coordinates provided.
+     * 
+     * @param points
+     *            The positions in the line string. There should be at least 2
+     *            positions for the document to be a valid LineString.
+     * 
+     * @return A GeoJSON LineString document from the coordinates provided.
+     * @throws IllegalArgumentException
+     *             If the list does not contain at least 2 points.
+     * @see <a
+     *      href="http://www.geojson.org/geojson-spec.html#linestring">GeoJSON
+     *      LineString</a>
+     */
+    public static final Document lineString(final List<? extends Point2D> points)
+            throws IllegalArgumentException {
+        if (points.size() < 2) {
+            throw new IllegalArgumentException(
+                    "A GeoJSON LineString must have at least 2 postions.");
+        }
+        final DocumentBuilder builder = BuilderFactory.start();
+        builder.add("type", "LineString");
+
+        final ArrayBuilder coordinates = builder.pushArray("coordinates");
+        add(coordinates, points);
+
+        return builder.build();
+    }
+
+    /**
+     * Constructs a GeoJSON 'LineString' document from the coordinates provided.
+     * 
+     * @param p1
+     *            The first position in the line string.
+     * @param p2
+     *            The second position in the line string.
+     * @param remaining
+     *            The remaining positions in the line string.
+     * 
+     * @return A GeoJSON LineString document from the coordinates provided.
+     * @see <a
+     *      href="http://www.geojson.org/geojson-spec.html#linestring">GeoJSON
+     *      LineString</a>
+     */
+    public static final Document lineString(final Point2D p1, final Point2D p2,
+            final Point2D... remaining) {
+        final DocumentBuilder builder = BuilderFactory.start();
+        builder.add("type", "LineString");
+        final ArrayBuilder coordinates = builder.pushArray("coordinates");
+
+        add(coordinates, p1);
+        add(coordinates, p2);
+        for (final Point2D point : remaining) {
+            add(coordinates, point);
+        }
+
+        return builder.build();
+    }
+
+    /**
+     * Constructs a GeoJSON 'MultiLineString' document from the coordinates
+     * provided.
+     * <p>
+     * Note: You will need to add a {@code @SuppressWarnings("unchecked")} to
+     * uses of this method as Java does not like mixing generics and varargs.
+     * The {@code @SafeVarargs} annotation is not available in Java 1.6, the
+     * minimum version for the driver.
+     * </p>
+     * 
+     * @param firstLineString
+     *            The first line string.
+     * @param additionalLineStrings
+     *            The remaining line strings.
+     * @return A GeoJSON MultiLineString document from the coordinates provided.
+     * 
+     * @see <a href="http://www.geojson.org/geojson-spec.html#polygon">GeoJSON
+     *      Polygon</a>
+     */
+    public static final Document multiLineString(
+            final List<? extends Point2D> firstLineString,
+            final List<? extends Point2D>... additionalLineStrings) {
+        final DocumentBuilder builder = BuilderFactory.start();
+        builder.add("type", "MultiLineString");
+
+        final ArrayBuilder coordinates = builder.pushArray("coordinates");
+
+        add(coordinates.pushArray(), firstLineString);
+
+        for (final List<? extends Point2D> additionalLineString : additionalLineStrings) {
+            add(coordinates.pushArray(), additionalLineString);
+        }
+
+        return builder.build();
+    }
+
+    /**
+     * Constructs a GeoJSON 'MultiPoint' document from the positions provided.
+     * 
+     * @param positions
+     *            The positions
+     * @return A GeoJSON MultiPoint document from the coordinates provided.
+     * @see <a href="http://www.geojson.org/geojson-spec.html#point">GeoJSON
+     *      Point</a>
+     */
+    public static final Document multiPoint(
+            final List<? extends Point2D> positions) {
+        final DocumentBuilder builder = BuilderFactory.start();
+        builder.add("type", "MultiPoint");
+
+        final ArrayBuilder coordinates = builder.pushArray("coordinates");
+        for (final Point2D position : positions) {
+            add(coordinates, position);
+
+        }
+        return builder.build();
+    }
+
+    /**
+     * Constructs a GeoJSON 'MultiPoint' document from the positions provided.
+     * 
+     * @param firstPosition
+     *            The first position
+     * @param additionalPositions
+     *            The other positions
+     * @return A GeoJSON MultiPoint document from the coordinates provided.
+     * @see <a href="http://www.geojson.org/geojson-spec.html#point">GeoJSON
+     *      Point</a>
+     */
+    public static final Document multiPoint(final Point2D firstPosition,
+            final Point2D... additionalPositions) {
+        final DocumentBuilder builder = BuilderFactory.start();
+        builder.add("type", "MultiPoint");
+
+        final ArrayBuilder coordinates = builder.pushArray("coordinates");
+        add(coordinates, firstPosition);
+        for (final Point2D additionalPosition : additionalPositions) {
+            add(coordinates, additionalPosition);
+
+        }
+        return builder.build();
+    }
+
+    /**
+     * Helper method to construct a {@link Point2D} from the (x, y) coordinates.
+     * 
+     * @param x
+     *            The point's x position
+     * @param y
+     *            The point's y position
+     * @return A Point for the coordinates provided.
+     */
+    public static final Point2D p(final double x, final double y) {
+        return new Point2D.Double(x, y);
+    }
+
+    /**
+     * Helper method to construct a {@link Point} from the (x, y) coordinates.
+     * 
+     * @param x
+     *            The point's x position
+     * @param y
+     *            The point's y position
+     * @return A Point for the coordinates provided.
+     */
+    public static final Point p(final int x, final int y) {
+        return new Point(x, y);
+    }
+
+    /**
+     * Constructs a GeoJSON 'Point' document from the coordinates provided.
+     * 
+     * @param position
+     *            The point's position
+     * @return A GeoJSON Point document from the coordinates provided.
+     * @see <a href="http://www.geojson.org/geojson-spec.html#point">GeoJSON
+     *      Point</a>
+     */
+    public static final Document point(final Point2D position) {
+        final DocumentBuilder builder = BuilderFactory.start();
+        builder.add("type", "Point");
+
+        if (position.getClass() == POINT_CLASS) {
+            final Point p = (Point) position;
+            builder.pushArray("coordinates").add(p.x).add(p.y);
+        }
+        else {
+            builder.pushArray("coordinates").add(position.getX())
+                    .add(position.getY());
+        }
+
+        return builder.build();
+    }
+
+    /**
+     * Constructs a GeoJSON 'Polygon' document from the coordinates provided.
+     * 
+     * @param boundary
+     *            The boundary positions for the polygon.
+     * @return A GeoJSON Polygon document from the coordinates provided.
+     * @throws IllegalArgumentException
+     *             If the line ring does not have at least 4 positions or the
+     *             first and last positions are not equivalent.
+     * 
+     * @see <a href="http://www.geojson.org/geojson-spec.html#polygon">GeoJSON
+     *      Polygon</a>
+     */
+    public static final Document polygon(final List<? extends Point2D> boundary)
+            throws IllegalArgumentException {
+        final DocumentBuilder builder = BuilderFactory.start();
+        builder.add("type", "Polygon");
+
+        final ArrayBuilder coordinates = builder.pushArray("coordinates");
+
+        lineRing(coordinates.pushArray(), boundary);
+
+        return builder.build();
+    }
+
+    /**
+     * Constructs a GeoJSON 'Polygon' document from the coordinates provided.
+     * <p>
+     * Note: You will need to add a {@code @SuppressWarnings("unchecked")} to
+     * uses of this method as Java does not like mixing generics and varargs.
+     * The {@code @SafeVarargs} annotation is not available in Java 1.6, the
+     * minimum version for the driver.
+     * </p>
+     * 
+     * @param boundary
+     *            The boundary positions for the polygon.
+     * @param holes
+     *            The positions for the holes within the polygon.
+     * @return A GeoJSON Polygon document from the coordinates provided.
+     * @throws IllegalArgumentException
+     *             If the line ring does not have at least 4 positions or the
+     *             first and last positions are not equivalent.
+     * 
+     * @see <a href="http://www.geojson.org/geojson-spec.html#polygon">GeoJSON
+     *      Polygon</a>
+     */
+    public static final Document polygon(
+            final List<? extends Point2D> boundary,
+            final List<? extends Point2D>... holes)
+            throws IllegalArgumentException {
+        final DocumentBuilder builder = BuilderFactory.start();
+        builder.add("type", "Polygon");
+
+        final ArrayBuilder coordinates = builder.pushArray("coordinates");
+
+        lineRing(coordinates.pushArray(), boundary);
+
+        for (final List<? extends Point2D> hole : holes) {
+            lineRing(coordinates.pushArray(), hole);
+        }
+
+        return builder.build();
+    }
+
+    /**
+     * Adds a positions to the coordinates array.
+     * 
+     * @param coordinates
+     *            The array to add the position to.
+     * @param positions
+     *            The positions to add.
+     */
+    protected static void add(final ArrayBuilder coordinates,
+            final List<? extends Point2D> positions) {
+        for (final Point2D position : positions) {
+            add(coordinates, position);
+        }
+    }
+
+    /**
+     * Adds a position to the coordinates array.
+     * 
+     * @param coordinates
+     *            The array to add the position to.
+     * @param position
+     *            The point to add.
+     */
+    protected static void add(final ArrayBuilder coordinates,
+            final Point2D position) {
+        if (position.getClass() == POINT_CLASS) {
+            final Point p = (Point) position;
+            coordinates.pushArray().add(p.x).add(p.y);
+        }
+        else {
+            coordinates.pushArray().add(position.getX()).add(position.getY());
+        }
+    }
+
+    /**
+     * Fills in the LineRing coordinates.
+     * 
+     * @param positionArray
+     *            The array to fill with the positions.
+     * @param positions
+     *            The positions defining the LineRing.
+     * @throws IllegalArgumentException
+     *             If the line ring does not have at least 4 positions or the
+     *             first and last positions are not equivalent.
+     */
+    protected static void lineRing(final ArrayBuilder positionArray,
+            final List<? extends Point2D> positions)
+            throws IllegalArgumentException {
+
+        if (positions.size() < 4) {
+            throw new IllegalArgumentException(
+                    "A GeoJSON LineRing must have at least 4 postions.");
+        }
+
+        final Point2D first = positions.get(0);
+        Point2D last = first;
+        for (final Point2D point : positions) {
+            add(positionArray, point);
+            last = point;
+        }
+        // The LineString must loop to form a LineRing.
+        if (!last.equals(first)) {
+            throw new IllegalArgumentException(
+                    "A GeoJSON LineRing's first and last postion must be equal.");
+        }
+    }
+
+    /**
+     * Creates a new GeoJson.
+     */
+    private GeoJson() { /* Static Class */
+    }
+}
---
+++ src/test/java/com/allanbank/mongodb/builder/GeoJsonTest.java
@@ -0,0 +1,211 @@
+/*
+ * Copyright 2013, Allanbank Consulting, Inc. 
+ *           All Rights Reserved
+ */
+
+package com.allanbank.mongodb.builder;
+
+import static com.allanbank.mongodb.builder.GeoJson.p;
+import static org.junit.Assert.assertEquals;
+
+import java.awt.Point;
+import java.awt.geom.Point2D;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.allanbank.mongodb.bson.Document;
+import com.allanbank.mongodb.bson.json.Json;
+
+/**
+ * GeoJsonTest provides tests for the {@link GeoJson} class.
+ * 
+ * @copyright 2013, Allanbank Consulting, Inc., All Rights Reserved
+ */
+public class GeoJsonTest {
+
+    /**
+     * Validates the example in the {@link GeoJson} class's JavaDoc.
+     */
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testClassJavaDoc() {
+        final String json = "{ 'type': 'Polygon',  'coordinates': [ "
+                + "[ [100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0] ],"
+                + "[ [100.2, 0.2], [100.8, 0.2], [100.8, 0.8], [100.2, 0.8], [100.2, 0.2] ] ] }";
+        final Document jsonDoc = Json.parse(json);
+
+        final Document geoJsonPolygon = GeoJson.polygon(
+                Arrays.asList(p(100.0, 0.0), p(101.0, 0.0), p(101.0, 1.0),
+                        p(100.0, 1.0), p(100.0, 0.0)),
+                Arrays.asList(p(100.2, 0.2), p(100.8, 0.2), p(100.8, 0.8),
+                        p(100.2, 0.8), p(100.2, 0.2)));
+
+        assertEquals(jsonDoc, geoJsonPolygon);
+    }
+
+    /**
+     * Test method for {@link GeoJson#lineString(List)}.
+     */
+    @Test
+    public void testLineStringListOfPoint2D() {
+        final String json = "{ 'type': 'LineString',  'coordinates': [ "
+                + " [1,2], [3,4], [5,6] ] }";
+        final Document jsonDoc = Json.parse(json);
+
+        final Document geoDoc = GeoJson.lineString(Arrays.asList(p(1, 2),
+                p(3, 4), p(5, 6)));
+
+        assertEquals(jsonDoc, geoDoc);
+    }
+
+    /**
+     * Test method for {@link GeoJson#lineString(List)}.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testLineStringListOfPoint2DTooFewItems() {
+        GeoJson.lineString(Arrays.asList(p(1, 2)));
+    }
+
+    /**
+     * Test method for {@link GeoJson#lineString(Point2D, Point2D, Point2D[])} .
+     */
+    @Test
+    public void testLineStringPoint2DPoint2DPoint2DArray() {
+        final String json = "{ 'type': 'LineString',  'coordinates': [ "
+                + " [1,2], [3,4], [5,6] ] }";
+        final Document jsonDoc = Json.parse(json);
+
+        final Document geoDoc = GeoJson.lineString(p(1, 2), p(3, 4), p(5, 6));
+
+        assertEquals(jsonDoc, geoDoc);
+    }
+
+    /**
+     * Test method for {@link GeoJson#multiLineString(List, List...)}.
+     */
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testMultiLineString() {
+        final String json = "{ 'type': 'MultiLineString',  'coordinates': [ "
+                + "[ [100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0] ],"
+                + "[ [100.2, 0.2], [100.8, 0.2], [100.8, 0.8], [100.2, 0.8], [100.2, 0.2] ] ] }";
+        final Document jsonDoc = Json.parse(json);
+
+        final Document geoJson = GeoJson.multiLineString(
+                Arrays.asList(p(100.0, 0.0), p(101.0, 0.0), p(101.0, 1.0),
+                        p(100.0, 1.0), p(100.0, 0.0)),
+                Arrays.asList(p(100.2, 0.2), p(100.8, 0.2), p(100.8, 0.8),
+                        p(100.2, 0.8), p(100.2, 0.2)));
+
+        assertEquals(jsonDoc, geoJson);
+    }
+
+    /**
+     * Test method for {@link GeoJson#multiPoint(List)}.
+     */
+    @Test
+    public void testMultiPointListOfPoint2D() {
+        final String json = "{ 'type': 'MultiPoint',  'coordinates': [ "
+                + "[100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0] ] }";
+        final Document jsonDoc = Json.parse(json);
+
+        final Document geoJson = GeoJson.multiPoint(Arrays.asList(
+                p(100.0, 0.0), p(101.0, 0.0), p(101.0, 1.0), p(100.0, 1.0),
+                p(100.0, 0.0)));
+
+        assertEquals(jsonDoc, geoJson);
+    }
+
+    /**
+     * Test method for {@link GeoJson#multiPoint(Point2D, Point2D[])} .
+     */
+    @Test
+    public void testMultiPointPoint2DPoint2DArray() {
+        final String json = "{ 'type': 'MultiPoint',  'coordinates': [ "
+                + "[100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0] ] }";
+        final Document jsonDoc = Json.parse(json);
+
+        final Document geoJson = GeoJson.multiPoint(p(100.0, 0.0),
+                p(101.0, 0.0), p(101.0, 1.0), p(100.0, 1.0), p(100.0, 0.0));
+
+        assertEquals(jsonDoc, geoJson);
+    }
+
+    /**
+     * Test method for {@link GeoJson#p(double, double)}.
+     */
+    @Test
+    public void testPDoubleDouble() {
+        assertEquals(new Point2D.Double(1.1, 2.2), GeoJson.p(1.1, 2.2));
+    }
+
+    /**
+     * Test method for {@link GeoJson#p(int, int)} .
+     */
+    @Test
+    public void testPIntInt() {
+        assertEquals(new Point(1, 2), GeoJson.p(1, 2));
+    }
+
+    /**
+     * Test method for {@link GeoJson#point(Point2D)} .
+     */
+    @Test
+    public void testPoint() {
+        final String json = "{ 'type': 'Point',  'coordinates': [5.1,6.2] }";
+        final Document jsonDoc = Json.parse(json);
+
+        final Document geoDoc = GeoJson.point(p(5.1, 6.2));
+
+        assertEquals(jsonDoc, geoDoc);
+    }
+
+    /**
+     * Test method for {@link GeoJson#point(Point2D)} .
+     */
+    @Test
+    public void testPointWithInts() {
+        final String json = "{ 'type': 'Point',  'coordinates': [5,6] }";
+        final Document jsonDoc = Json.parse(json);
+
+        final Document geoDoc = GeoJson.point(p(5, 6));
+
+        assertEquals(jsonDoc, geoDoc);
+    }
+
+    /**
+     * Test method for {@link GeoJson#polygon(List)}.
+     */
+    @Test
+    public void testPolygon() {
+        final String json = "{ 'type': 'Polygon',  'coordinates': [ "
+                + "[ [100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0] ] ] }";
+        final Document jsonDoc = Json.parse(json);
+
+        final Document geoJsonPolygon = GeoJson.polygon(Arrays.asList(
+                p(100.0, 0.0), p(101.0, 0.0), p(101.0, 1.0), p(100.0, 1.0),
+                p(100.0, 0.0)));
+
+        assertEquals(jsonDoc, geoJsonPolygon);
+    }
+
+    /**
+     * Test method for {@link GeoJson#polygon(List)}.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testPolygonNotClosed() {
+        GeoJson.polygon(Arrays.asList(p(100.0, 0.0), p(101.0, 0.0),
+                p(101.0, 1.0), p(100.0, 1.0), p(100.0, 0.1)));
+    }
+
+    /**
+     * Test method for {@link GeoJson#polygon(List)}.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testPolygonTooSmall() {
+        GeoJson.polygon(Arrays.asList(p(100.0, 0.0), p(101.0, 0.0),
+                p(101.0, 1.0)));
+    }
+}
