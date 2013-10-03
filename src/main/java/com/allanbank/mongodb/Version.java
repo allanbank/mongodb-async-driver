39c1269b90bc781304895fd7d0519ae76b2ddded
Robert J. Moore <Robert.J.Moore@allanbank.com>
2013-10-03 19:51:20 -0400

    Add a version class.

---
+++ src/main/java/com/allanbank/mongodb/Version.java
@@ -0,0 +1,238 @@
+/*
+ * Copyright 2013, Allanbank Consulting, Inc. 
+ *           All Rights Reserved
+ */
+
+package com.allanbank.mongodb;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+import com.allanbank.mongodb.bson.NumericElement;
+import com.allanbank.mongodb.util.IOUtils;
+
+/**
+ * Version provides TODO - Finish
+ * 
+ * @copyright 2013, Allanbank Consulting, Inc., All Rights Reserved
+ */
+public class Version implements Serializable, Comparable<Version> {
+
+    /** The driver's version. */
+    public static final Version VERSION;
+
+    /** The logger for the {@link Version}. */
+    private static final Logger LOG = Logger.getLogger(Version.class
+            .getCanonicalName());
+
+    /** The serialization version for the class. */
+    private static final long serialVersionUID = 4726973040107711788L;
+
+    static {
+        // Load the version from the maven pom.properties file.
+        Properties props = new Properties();
+        InputStream in = null;
+        try {
+            in = Version.class
+                    .getResourceAsStream("/META-INF/maven/com.allanbank/"
+                            + "mongodb-async-driver/pom.properties");
+            if (in != null) {
+                props.load(in);
+            }
+        }
+        catch (IOException error) {
+            LOG.info("Could not read the version information for the driver.");
+        }
+        finally {
+            IOUtils.close(in);
+        }
+
+        VERSION = parse(props.getProperty("version", "0-DEVELOPMENT"));
+    }
+
+    /**
+     * Parses a version of the general format 'int.int.int-suffix'.
+     * 
+     * @param version
+     *            The version string to parse.
+     * @return The version.
+     */
+    public static Version parse(String version) {
+
+        String[] tokens = version.split("\\.");
+        int[] versions = new int[tokens.length];
+        String suffix = "";
+        for (int i = 0; i < tokens.length; ++i) {
+            String token = tokens[i];
+
+            // Check for a suffix on the last token.
+            if (i == (tokens.length - 1)) {
+                int dashIndex = token.indexOf('-');
+                if (dashIndex >= 0) {
+                    suffix = token.substring(dashIndex + 1);
+                    token = token.substring(0, dashIndex);
+                }
+            }
+
+            try {
+                versions[i] = Integer.parseInt(token);
+            }
+            catch (NumberFormatException nfe) {
+                LOG.fine("Could not parse version string token ('" + token
+                        + "') from version '" + version + "'.");
+            }
+        }
+
+        return new Version(versions, suffix);
+    }
+
+    /**
+     * Parses a version from a version array. The values in the array are
+     * assumed to be {@link NumericElement}s.
+     * 
+     * @param versionArray
+     *            The version array to parse.
+     * @return The version.
+     */
+    public static Version parse(List<NumericElement> versionArray) {
+
+        int[] version = new int[versionArray.size()];
+        for (int i = 0; i < version.length; ++i) {
+            version[i] = versionArray.get(i).getIntValue();
+        }
+
+        return new Version(version, null);
+    }
+
+    /** The suffix for the version like 'SNAPSHOT'. May be the empty string. */
+    private String mySuffix;
+
+    /** The "values" for the version number. */
+    private int[] myVersion;
+
+    /**
+     * Creates a new Version.
+     * 
+     * @param version
+     *            The "values" for the version number.
+     * @param suffix
+     *            The suffix for the version like 'SNAPSHOT'. May be the empty
+     *            string.
+     */
+    private Version(int[] version, String suffix) {
+        myVersion = version.clone();
+        mySuffix = (suffix != null) ? suffix : "";
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Overridden to compare the two versions.
+     * </p>
+     */
+    @Override
+    public int compareTo(Version other) {
+
+        int compare = 0;
+
+        int fields = Math.min(myVersion.length, other.myVersion.length);
+        for (int i = 0; (compare == 0) && (i < fields); ++i) {
+            compare = compare(myVersion[i], other.myVersion[i]);
+        }
+
+        if (compare == 0) {
+            compare = compare(myVersion.length, other.myVersion.length);
+            if (compare == 0) {
+                compare = mySuffix.compareTo(other.mySuffix);
+            }
+        }
+
+        return compare;
+    }
+
+    /**
+     * Determines if the passed object is of this same type as this object and
+     * if so that its fields are equal.
+     * 
+     * @param object
+     *            The object to compare to.
+     * 
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object object) {
+        boolean result = false;
+        if (this == object) {
+            result = true;
+        }
+        else if ((object != null) && (getClass() == object.getClass())) {
+            final Version other = (Version) object;
+
+            result = mySuffix.equals(other.mySuffix)
+                    && Arrays.equals(myVersion, other.myVersion);
+        }
+        return result;
+    }
+
+    /**
+     * Computes a reasonable hash code.
+     * 
+     * @return The hash code value.
+     */
+    @Override
+    public int hashCode() {
+        int result = 1;
+        result = 31 * result + mySuffix.hashCode();
+        result = 31 * result + Arrays.hashCode(myVersion);
+        return result;
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Overridden to produce a human readable string for the version.
+     * </p>
+     */
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < myVersion.length; ++i) {
+            if (i != 0) {
+                builder.append('.');
+            }
+            builder.append(String.valueOf(myVersion[i]));
+        }
+        if (!mySuffix.isEmpty()) {
+            builder.append('-');
+            builder.append(mySuffix);
+        }
+        return builder.toString();
+    }
+
+    /**
+     * Compares two {@code int} values numerically. The value returned is
+     * identical to what would be returned by:
+     * 
+     * <pre>
+     * Integer.valueOf(x).compareTo(Integer.valueOf(y))
+     * </pre>
+     * 
+     * @param x
+     *            the first {@code int} to compare
+     * @param y
+     *            the second {@code int} to compare
+     * @return the value {@code 0} if {@code x == y}; a value less than
+     *         {@code 0} if {@code x < y}; and a value greater than {@code 0} if
+     *         {@code x > y}
+     * @since 1.7
+     */
+    protected int compare(final int x, final int y) {
+        return (x < y) ? -1 : ((x == y) ? 0 : 1);
+    }
+
+}
---
+++ src/test/java/com/allanbank/mongodb/VersionTest.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright 2013, Allanbank Consulting, Inc. 
+ *           All Rights Reserved
+ */
+
+package com.allanbank.mongodb;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.allanbank.mongodb.bson.NumericElement;
+import com.allanbank.mongodb.bson.element.DoubleElement;
+import com.allanbank.mongodb.bson.element.IntegerElement;
+import com.allanbank.mongodb.bson.element.LongElement;
+
+/**
+ * VersionTest provides tests for the {@link Version} class.
+ * 
+ * @copyright 2013, Allanbank Consulting, Inc., All Rights Reserved
+ */
+public class VersionTest {
+
+    /**
+     * Test method for {@link Version#compareTo(com.allanbank.mongodb.Version)}.
+     */
+    @Test
+    public void testCompareTo() {
+        final String[] order = new String[] { "1", "1.2", "1.2.3", "2.1.1",
+                "2.1.1-S", "2.1.1-T" };
+
+        for (int i = 0; i < order.length; ++i) {
+            for (int j = i; j < order.length; ++j) {
+                final Version ith = Version.parse(order[i]);
+                final Version jth = Version.parse(order[j]);
+                if (i == j) {
+                    assertThat(ith + " == " + jth, ith.compareTo(jth), is(0));
+                    assertThat(jth + " == " + ith, jth.compareTo(ith), is(0));
+                }
+                else {
+                    assertThat(ith + " < " + jth, ith.compareTo(jth),
+                            lessThan(0));
+                    assertThat(jth + " < " + ith, jth.compareTo(ith),
+                            greaterThan(0));
+                }
+            }
+        }
+    }
+
+    /**
+     * Test method for {@link Version#VERSION}.
+     */
+    @Test
+    public void testAutoVersion() {
+        assertThat(Version.VERSION, notNullValue());
+    }
+
+    /**
+     * Test method for {@link Version#equals} and {@link Version#hashCode}.
+     */
+    @Test
+    public void testEqualsAndHashCode() {
+        final List<Version> objs1 = new ArrayList<Version>();
+        final List<Version> objs2 = new ArrayList<Version>();
+
+        final String[] versions = new String[] { "1", "1.2", "1.2.3",
+                "1-SNAPSHOT", "1.2-SNAPSHOT", "1.2.3-SNAPSHOT", "",
+                String.valueOf(Integer.MAX_VALUE),
+                String.valueOf(Integer.MIN_VALUE) };
+
+        for (final String version : versions) {
+            objs1.add(Version.parse(version));
+            objs2.add(Version.parse(version));
+        }
+
+        // Sanity check.
+        assertEquals(objs1.size(), objs2.size());
+
+        for (int i = 0; i < objs1.size(); ++i) {
+            final Version obj1 = objs1.get(i);
+            Version obj2 = objs2.get(i);
+
+            assertTrue(obj1.equals(obj1));
+            assertEquals(obj1, obj2);
+
+            assertEquals(obj1.hashCode(), obj2.hashCode());
+
+            for (int j = i + 1; j < objs1.size(); ++j) {
+                obj2 = objs2.get(j);
+
+                assertFalse(obj1.equals(obj2));
+                assertFalse(obj1.hashCode() == obj2.hashCode());
+            }
+
+            assertFalse(obj1.equals("foo"));
+            assertFalse(obj1.equals(null));
+        }
+    }
+
+    /**
+     * Test method for {@link Version#parse(java.util.List)}.
+     */
+    @Test
+    public void testParseListOfNumericElement() {
+
+        final List<NumericElement> items = new ArrayList<NumericElement>();
+        items.add(new IntegerElement("g", 1));
+        items.add(new LongElement("g", 2));
+        items.add(new DoubleElement("g", 3));
+
+        assertThat(Version.parse(items), is(Version.parse("1.2.3")));
+    }
+
+    /**
+     * Test method for {@link Version#toString()}.
+     */
+    @Test
+    public void testToString() {
+        assertThat(Version.parse("1.2.3.4.5").toString(), is("1.2.3.4.5"));
+        assertThat(Version.parse("1.2.3.4.5-FOO").toString(),
+                is("1.2.3.4.5-FOO"));
+        assertThat(Version.parse("1-FOO").toString(), is("1-FOO"));
+    }
+
+}
