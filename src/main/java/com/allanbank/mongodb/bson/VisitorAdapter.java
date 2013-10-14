2f7a285c10d69236fd3bbd12e966d947e73a5586
Robert J. Moore <Robert.J.Moore@allanbank.com>
2013-10-14 13:59:56 -0400

    VisitorAdapter implementation.

---
+++ src/main/java/com/allanbank/mongodb/bson/VisitorAdapter.java
@@ -0,0 +1,294 @@
+/*
+ * Copyright 2013, Allanbank Consulting, Inc. 
+ *           All Rights Reserved
+ */
+
+package com.allanbank.mongodb.bson;
+
+import java.util.List;
+
+import com.allanbank.mongodb.bson.element.ObjectId;
+
+/**
+ * VisitorAdapter provides a helper for {@link Visitor} implementations that are
+ * only interested in a subset of the elements within a document.
+ * <p>
+ * This implementation will walk the entire tree of elements. Derived classes
+ * need only override the methods of interest. Derived classes should be careful
+ * to always call the {@code super} implementation of the following methods to
+ * ensure they do not break the document walking. Calling {@code super} for all
+ * methods is encouraged.
+ * <ul>
+ * <li> {@link #visit(List)}</li>
+ * <li> {@link #visitArray(String, List)}</li>
+ * <li> {@link #visitDocument(String, List)}</li>
+ * <li> {@link #visitJavaScript(String, String, Document)}</li>
+ * </ul>
+ * </p>
+ * <p>
+ * As a further aid to {@link Visitor} implementations only interested in the
+ * names of elements, this class will call the {@link #visitName(String)} method
+ * for each element visited.
+ * </p>
+ * 
+ * @api.yes This class is part of the driver's API. Public and protected members
+ *          will be deprecated for at least 1 non-bugfix release (version
+ *          numbers are &lt;major&gt;.&lt;minor&gt;.&lt;bugfix&gt;) before being
+ *          removed or modified.
+ * @copyright 2013, Allanbank Consulting, Inc., All Rights Reserved
+ */
+public class VisitorAdapter implements Visitor {
+
+    /**
+     * Creates a new VisitorAdapter.
+     */
+    public VisitorAdapter() {
+        super();
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Overridden to iterate over the elements of the document.
+     * </p>
+     */
+    @Override
+    public void visit(final List<Element> elements) {
+        for (final Element element : elements) {
+            element.accept(this);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Overridden to visit the name of the element and then iterate over the
+     * elements of the array.
+     * </p>
+     */
+    @Override
+    public void visitArray(final String name, final List<Element> elements) {
+        visitName(name);
+        for (final Element element : elements) {
+            element.accept(this);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Overridden to visit the name of the element.
+     * </p>
+     */
+    @Override
+    public void visitBinary(final String name, final byte subType,
+            final byte[] data) {
+        visitName(name);
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Overridden to visit the name of the element.
+     * </p>
+     */
+    @Override
+    public void visitBoolean(final String name, final boolean value) {
+        visitName(name);
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Overridden to visit the name of the element.
+     * </p>
+     */
+    @Override
+    public void visitDBPointer(final String name, final String databaseName,
+            final String collectionName, final ObjectId id) {
+        visitName(name);
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Overridden to visit the name of the element and then iterate over the
+     * elements of the document element.
+     * </p>
+     */
+    @Override
+    public void visitDocument(final String name, final List<Element> elements) {
+        visitName(name);
+        for (final Element element : elements) {
+            element.accept(this);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Overridden to visit the name of the element.
+     * </p>
+     */
+    @Override
+    public void visitDouble(final String name, final double value) {
+        visitName(name);
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Overridden to visit the name of the element.
+     * </p>
+     */
+    @Override
+    public void visitInteger(final String name, final int value) {
+        visitName(name);
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Overridden to visit the name of the element.
+     * </p>
+     */
+    @Override
+    public void visitJavaScript(final String name, final String code) {
+        visitName(name);
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Overridden to visit the name of the element.
+     * </p>
+     */
+    @Override
+    public void visitJavaScript(final String name, final String code,
+            final Document scope) {
+        visitName(name);
+        scope.accept(this);
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Overridden to visit the name of the element.
+     * </p>
+     */
+    @Override
+    public void visitLong(final String name, final long value) {
+        visitName(name);
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Overridden to visit the name of the element.
+     * </p>
+     */
+    @Override
+    public void visitMaxKey(final String name) {
+        visitName(name);
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Overridden to visit the name of the element.
+     * </p>
+     */
+    @Override
+    public void visitMinKey(final String name) {
+        visitName(name);
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Overridden to visit the name of the element.
+     * </p>
+     */
+    @Override
+    public void visitMongoTimestamp(final String name, final long value) {
+        visitName(name);
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Overridden to visit the name of the element.
+     * </p>
+     */
+    @Override
+    public void visitNull(final String name) {
+        visitName(name);
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Overridden to visit the name of the element.
+     * </p>
+     */
+    @Override
+    public void visitObjectId(final String name, final ObjectId id) {
+        visitName(name);
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Overridden to visit the name of the element.
+     * </p>
+     */
+    @Override
+    public void visitRegularExpression(final String name, final String pattern,
+            final String options) {
+        visitName(name);
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Overridden to visit the name of the element.
+     * </p>
+     */
+    @Override
+    public void visitString(final String name, final String value) {
+        visitName(name);
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Overridden to visit the name of the element.
+     * </p>
+     */
+    @Override
+    public void visitSymbol(final String name, final String symbol) {
+        visitName(name);
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Overridden to visit the name of the element.
+     * </p>
+     */
+    @Override
+    public void visitTimestamp(final String name, final long timestamp) {
+        visitName(name);
+    }
+
+    /**
+     * Extension point for {@link Visitor} implementation only interested in the
+     * name of the elements.
+     * 
+     * @param name
+     *            The name of the element.
+     */
+    protected void visitName(final String name) {
+        // Nothing. Extension point.
+    }
+}
---
+++ src/test/java/com/allanbank/mongodb/bson/VisitorAdapterTest.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2013, Allanbank Consulting, Inc. 
+ *           All Rights Reserved
+ */
+
+package com.allanbank.mongodb.bson;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.allanbank.mongodb.bson.builder.ArrayBuilder;
+import com.allanbank.mongodb.bson.builder.BuilderFactory;
+import com.allanbank.mongodb.bson.builder.DocumentBuilder;
+import com.allanbank.mongodb.bson.element.BooleanElement;
+import com.allanbank.mongodb.bson.element.ObjectId;
+
+/**
+ * VisitorAdapterTest provides tests for the {@link VisitorAdapter}.
+ * 
+ * @copyright 2013, Allanbank Consulting, Inc., All Rights Reserved
+ */
+public class VisitorAdapterTest {
+
+    /**
+     * Test method for {@link VisitorAdapter#visit(java.util.List)}.
+     */
+    @SuppressWarnings("deprecation")
+    @Test
+    public void testVisit() {
+        DocumentBuilder builder = BuilderFactory.start();
+
+        builder.addBoolean("true", true);
+        final Document simple = builder.build();
+
+        builder = BuilderFactory.start();
+        builder.add(new BooleanElement("_id", false));
+        builder.addBinary("binary", new byte[20]);
+        builder.addBinary("binary-2", (byte) 2, new byte[40]);
+        builder.addBoolean("true", true);
+        builder.addBoolean("false", false);
+        builder.addDBPointer("DBPointer", "db", "collection", new ObjectId(1,
+                2L));
+        builder.addDouble("double", 4884.45345);
+        builder.addDocument("simple", simple);
+        builder.addInteger("int", 123456);
+        builder.addJavaScript("javascript", "function foo() { }");
+        builder.addJavaScript("javascript_with_code", "function foo() { }",
+                simple);
+        builder.addLong("long", 1234567890L);
+        builder.addMaxKey("max");
+        builder.addMinKey("min");
+        builder.addMongoTimestamp("mongo-time", System.currentTimeMillis());
+        builder.addNull("null");
+        builder.addObjectId("object-id",
+                new ObjectId((int) System.currentTimeMillis() / 1000, 1234L));
+        builder.addRegularExpression("regex", ".*", "");
+        builder.addString("string", "string\u0090\ufffe");
+        builder.addSymbol("symbol", "symbol");
+        builder.addTimestamp("timestamp", System.currentTimeMillis());
+        builder.push("sub-doc").addBoolean("true", true).pop();
+
+        final ArrayBuilder aBuilder = builder.pushArray("array");
+        aBuilder.addBinary(new byte[20]);
+        aBuilder.addBinary((byte) 2, new byte[40]);
+        aBuilder.addBoolean(true);
+        aBuilder.addBoolean(false);
+        aBuilder.addDBPointer("db", "collection", new ObjectId(1, 2L));
+        aBuilder.addDouble(4884.45345);
+        aBuilder.addInteger(123456);
+        aBuilder.addJavaScript("function foo() { }");
+        aBuilder.addJavaScript("function foo() { }", simple);
+        aBuilder.addLong(1234567890L);
+        aBuilder.addMaxKey();
+        aBuilder.addMinKey();
+        aBuilder.addMongoTimestamp(System.currentTimeMillis());
+        aBuilder.addNull();
+        aBuilder.addObjectId(new ObjectId(
+                (int) System.currentTimeMillis() / 1000, 1234L));
+        aBuilder.addRegularExpression(".*", "");
+        aBuilder.addString("string");
+        aBuilder.addSymbol("symbol");
+        aBuilder.addTimestamp(System.currentTimeMillis());
+        aBuilder.push().addBoolean("true", true).pop();
+
+        final Document doc = builder.build();
+
+        final List<String> expected = Arrays.asList("_id", "binary",
+                "binary-2", "true", "false", "DBPointer", "double", "simple",
+                "true", "int", "javascript", "javascript_with_code", "true",
+                "long", "max", "min", "mongo-time", "null", "object-id",
+                "regex", "string", "symbol", "timestamp",
+                // Sub-document with just a boolean.
+                "sub-doc", "true", "array",
+                // Array Elements.
+                "0", "1", "2", "3", "4", "5", "6", "7", "8", "true", "9", "10",
+                "11", "12", "13", "14", "15", "16", "17", "18", "19",
+                // Final document with just a boolean.
+                "true");
+
+        final List<String> names = new ArrayList<String>();
+        final VisitorAdapter adapter = new VisitorAdapter() {
+            @Override
+            public void visitName(final String name) {
+                super.visitName(name);
+                names.add(name);
+            }
+        };
+
+        doc.accept(adapter);
+
+        assertThat(names, is(expected));
+    }
+}
